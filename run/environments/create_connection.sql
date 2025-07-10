9b27007e0a2f6559665b2621914afd54c5a4f354
Sydney Nurse <sydney.nurse@oracle.com>
2025-07-10 10:47:56 +0000

    updated create connection script with output statements

--- run/environments/create_connection.sql
+++ run/environments/create_connection.sql
@@ -3,9 +3,11 @@ ACCEPT conn_name    CHAR PROMPT 'Enter the name of the connection to be saved:'
 ACCEPT conn_string  CHAR PROMPT 'Enter the database connection string:'
 
 -- Create a new SQLcl connection
+!echo 'Creating Connection ...'
 conn -savepwd -save &conn_name &conn_string
  
 --List current connections
+!echo 'List connections'
 cm list
 
 -- How to use the new connection
